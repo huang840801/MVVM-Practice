390ea619cfb0dedc059e52f628b7079575e247e5
M7274 <guanhong.huang@colatour.com.tw>
2019-07-30 08:41:44 +0800

    test

--- app/src/main/java/com/guanhong/mvvmpractice/MainActivity.kt
+++ app/src/main/java/com/guanhong/mvvmpractice/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 //        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        
+//        test
         getAllPlayer()
     }
 
