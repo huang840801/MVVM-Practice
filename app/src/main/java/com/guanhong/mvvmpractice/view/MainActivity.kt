01cb82f48a8adab9b1b9bbbd38ef6cd6a99c041c
GuanHongHuang <david840801@gmail.com>
2019-08-10 09:16:34 +0800

    Solve the bug that databinding and livedata get into infinite loop.

--- app/src/main/java/com/guanhong/mvvmpractice/view/MainActivity.kt
+++ app/src/main/java/com/guanhong/mvvmpractice/view/MainActivity.kt
@@ -3,20 +3,14 @@ package com.guanhong.mvvmpractice.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.Observable
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.guanhong.mvvmpractice.R
 import com.guanhong.mvvmpractice.databinding.ActivityMainBinding
-import com.guanhong.mvvmpractice.response.player.DataItem
 import com.guanhong.mvvmpractice.viewmodel.MainViewModel
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,33 +27,12 @@ class MainActivity : AppCompatActivity() {
 
 //        binding 的第二種方法
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-//        binding.lifecycleOwner = this    // set lifecycle owner for Livedata in xml
+        binding.lifecycleOwner = this    // set lifecycle owner for Livedata in xml
 
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         binding.viewModel = viewModel
 
-//        viewModel.getDataItem().observe(this,
-//            Observer<DataItem> { dataItem ->
-//
-//                            Log.d("Huang", " Observer "+ dataItem.firstName)
-//
-//                viewModel.dataItem.value = dataItem })
-
-
-        viewModel.test.observe(this,
-            Observer<String> { test ->
-
-                            Log.d("Huang", " Observer "+ test)
-
-                name.text = test
-//                viewModel.dataItem.value = test
-            })
-
-        viewModel.Test()
-
-
-//        viewModel.init()
-
+        viewModel.init()
     }
 
     fun onViewClick(view: View) {
--- app/src/main/java/com/guanhong/mvvmpractice/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/guanhong/mvvmpractice/viewmodel/MainViewModel.kt
@@ -1,90 +1,43 @@
 package com.guanhong.mvvmpractice.viewmodel
 
-import android.util.Log
 import androidx.databinding.ObservableBoolean
-import androidx.databinding.ObservableField
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.guanhong.mvvmpractice.callback.GetAllPlayerCallback
 import com.guanhong.mvvmpractice.repository.MainRepository
 import com.guanhong.mvvmpractice.response.player.DataItem
-import com.guanhong.mvvmpractice.response.player.Team
 
 class MainViewModel : ViewModel() {
 
-    private val repository = MainRepository()
-
-//    val data = ObservableField<List<DataItem>>()
-//    val dataItem = ObservableField<DataItem>()
-
-    //    val data = MutableLiveData<List<DataItem>>()
-    var dataItem = MutableLiveData<DataItem>()
-
+    val dataItem = MutableLiveData<DataItem>()
     val isLoading = ObservableBoolean()
-
-    var test = MutableLiveData<String>()
-    var num = 0
 
     private var playerNo = 0
 
+    private val repository = MainRepository()
+
     fun refresh() {
 
-        Log.d("Huang", " MainViewModel refresh")
-
-        setTestValue()
-//        getAllPlayer()
+        getAllPlayer()
     }
 
     fun init() {
         getAllPlayer()
     }
 
-    fun getDataItem(): LiveData<DataItem> {
-
-        return dataItem
-    }
-
     private fun getAllPlayer() {
 
-        Log.d("Huang", " MainViewModel getAllPlayer")
-
         isLoading.set(true)
-        val team = Team()
-
-//        val test = DataItem(0, 0, 0, "aaa", 0, "", team, " sss", "")
-
-//        dataItem.postValue(test)
 
         repository.getAllPlayer(object : GetAllPlayerCallback {
             override fun onSuccess(dataItemList: List<DataItem>) {
 
-                Log.d("Huang", " onSuccess " + dataItemList[0].firstName)
-                Log.d("Huang", " onSuccess " + dataItemList[0].lastName)
-                Log.d("Huang", " onSuccess " + dataItemList.count())
-
-//                data.postValue(dataItemList)
                 dataItem.value = (dataItemList[playerNo])
-
-//                data.value = dataItemList
-//                dataItem.value = dataItemList[playerNo]
-//                data.set(dataItemList)
-//                dataItem.set(dataItemList[playerNo])
+//                dataItem.postValue (dataItemList[playerNo])
 
                 isLoading.set(false)
                 playerNo++
             }
         })
-    }
-
-    fun Test() {
-        setTestValue()
-    }
-
-    fun setTestValue() {
-        test.value = "Test" + num
-
-        num++
     }
 }
