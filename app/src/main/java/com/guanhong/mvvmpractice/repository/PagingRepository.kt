1fdbef915f232d246f65c151a18f5f4c1bcd3727
M7274 <guanhong.huang@colatour.com.tw>
2019-09-11 17:51:01 +0800

    data source refactor

--- app/src/main/java/com/guanhong/mvvmpractice/repository/PagingRepository.kt
+++ app/src/main/java/com/guanhong/mvvmpractice/repository/PagingRepository.kt
@@ -2,30 +2,25 @@ package com.guanhong.mvvmpractice.repository
 
 import android.app.Application
 import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
 import com.guanhong.mvvmpractice.callback.PagingBoundaryCallback
-import com.guanhong.mvvmpractice.database.DataItemDao
 import com.guanhong.mvvmpractice.database.DataItemDbHelper
 import com.guanhong.mvvmpractice.datasource.DataSourceFactory
 import com.guanhong.mvvmpractice.response.player.DataItem
 
 class PagingRepository : PagingRepositoryCallback {
 
-    private val remoteSourceFactory by lazy {
-        DataSourceFactory()
-    }
-
-    private lateinit var localSourceFactory: DataItemDao
+    private lateinit var remoteDataSource: DataSourceFactory
+    private lateinit var localDataSource: DataSource.Factory<Int, DataItem>
 
     override fun getDataItem(application: Application): LiveData<PagedList<DataItem>> {
 
-        val dao = DataItemDbHelper(application).getRoomDataItemDao()
-
         val pagedListLiveData: LiveData<PagedList<DataItem>> by lazy {
 
-            val localDataSource = dao.getAllDataItem()
-            val remoteDataSource = remoteSourceFactory
+            localDataSource = DataItemDbHelper(application).getRoomDataItemDao().getAllDataItem()
+            remoteDataSource = DataSourceFactory()
 
             val config = PagedList.Config.Builder()
                 .setPageSize(25)
@@ -33,7 +28,7 @@ class PagingRepository : PagingRepositoryCallback {
 //                .setInitialLoadSizeHint(5)
                 .build()
 
-            LivePagedListBuilder(remoteDataSource, config)
+            LivePagedListBuilder(localDataSource, config)
                 .setBoundaryCallback(PagingBoundaryCallback(application))
                 .build()
         }
